1f323c156acc148d685551067bdc913132aa2451
Atharva0418 <atharvadholakia2004@gmail.com>
2026-07-22 20:31:24 +0530

    fix: change generation strategy for vehicle id

--- src/main/java/com/atharva/dealership/vehicle/Vehicle.java
+++ src/main/java/com/atharva/dealership/vehicle/Vehicle.java
@@ -3,6 +3,7 @@ package com.atharva.dealership.vehicle;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
 
@@ -10,7 +11,7 @@ import java.math.BigDecimal;
 public class Vehicle {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String make;
